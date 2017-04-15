f5e9be6b9f4a27d01037efcc9bab65ec5415f78e
Aman Saurabh <Aman Saurabh>
2017-04-15 11:42:01 +0530

    PNR layout and PNRPop

---
+++ LocationPlusAlarm/app/src/main/java/com/example/abhisheikh/locationplusalarm/activity/PNRPop.java
@@ -0,0 +1,15 @@
+package com.example.abhisheikh.locationplusalarm.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.abhisheikh.locationplusalarm.R;
+
+public class PNRPop extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pnrpop);
+    }
+}
