ffa8bec42e25cb45c7cf11b988ceea159dd41a3a
Aman Saurabh <Aman Saurabh>
2017-04-16 03:14:11 +0530

    Ringing Activity layout

---
+++ LocationPlusAlarm/app/src/main/java/com/example/abhisheikh/locationplusalarm/activity/RingingActivity.java
@@ -0,0 +1,15 @@
+package com.example.abhisheikh.locationplusalarm.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.abhisheikh.locationplusalarm.R;
+
+public class RingingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ringing);
+    }
+}
